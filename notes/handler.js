af098f2d9d950eeab118e72d6293cb0372f8821b
chamchop <ocfmiller@protonmail.com>
2023-04-18 12:19:34 +0100

    deploy configuration

--- notes/handler.js
+++ notes/handler.js
@@ -87,7 +87,6 @@ module.exports.deleteNote = async (event, context, cb) => {
 };
 
 module.exports.getAllNotes = async (event, context, cb) => {
-  console.log(JSON.stringify(event));
   context.callbackWaitsForEmptyEventLoop = false;
   try {
     const params = {
