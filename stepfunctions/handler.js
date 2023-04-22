6efd9f6c4bcfd4ce92d6f09cd7260ea7a3e441a7
chamchop <ocfmiller@protonmail.com>
2023-04-22 10:15:30 +0100

    inventory handlers

--- stepfunctions/handler.js
+++ stepfunctions/handler.js
@@ -1,18 +1,44 @@
-'use strict';
+const AWS = require("aws-sdk");
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DocumentClient = new DynamoDB.DocumentClient({region:'eu-west-2'});
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const isBookAvailable = (book, quantity) => {
+  return (book.quantity - quantity) > 0
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.checkInventory = async ({bookId, quantity}) => {
+  try {
+    let params = {
+      TableName: 'bookTable',
+      KeyConditionExpression: 'bookId = :bookId',
+      ExpressionAttributeValues: {
+        ':bookId': bookId
+      }
+    }
+
+    let result = await DocumentClient.query(params).promise();
+    let book = result.Items[0];
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("book out of stock");
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+
+  } catch(e) {
+    if(e.name == 'BookOutOfStock') {
+      throw e;
+    }
+    else {
+      let bookNotFoundError = new Error(e);
+      bookNotFoundError.name = 'BookNotFound';
+      throw e;
+    }
+  }
+};
+
+module.exports.calculateTotal = async (event) => {
+  return 100;
 };
