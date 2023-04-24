e3db7f8471ef5ea875c1003c8c17973467c302d7
chamchop <ocfmiller@protonmail.com>
2023-04-24 16:02:44 +0100

    adding handlers and states

--- stepfunctions/handler.js
+++ stepfunctions/handler.js
@@ -3,7 +3,8 @@ const DynamoDB = require("aws-sdk/clients/dynamodb");
 const DocumentClient = new DynamoDB.DocumentClient({region:'eu-west-2'});
 
 const isBookAvailable = (book, quantity) => {
-  return (book.quantity - quantity) > 0
+  // return (book.quantity - quantity) > 0
+  return quantity;
 }
 
 module.exports.checkInventory = async ({bookId, quantity}) => {
@@ -39,6 +40,67 @@ module.exports.checkInventory = async ({bookId, quantity}) => {
   }
 };
 
-module.exports.calculateTotal = async (event) => {
-  return 100;
+module.exports.calculateTotal = async ({ book, quantity }) => {
+  let total = book.price * quantity;
+  return { total };
 };
+
+const deductPoints = async (userId) => {
+  let params = {
+    TableName: 'userTable',
+    Key: {'userId': userId},
+    UpdateExpression: 'SET points =:zero',
+    ExpressionAttributeValues: {
+      ':zero': 0
+    }
+  }
+  await DocumentClient.update(params).promise();
+};
+
+module.exports.redeemPoints = async ({ userId, total }) => {
+  let orderTotal = total.total;
+  try {
+    let params = {
+      TableName: 'userTable',
+      Key: {
+        'userId': userId
+      }
+    }
+      let result = await DocumentClient.get(params).promise();
+      let user = result.Item;
+      
+      const points = user.points;
+      if (orderTotal > points) {
+        await deductPoints(userId);
+        orderTotal = orderTotal - points;
+        return { total: orderTotal, points }
+      } else {
+        throw new Error('order less than redeemed points');
+      }
+    } catch (e) {
+      throw new Error(e);
+  }
+};
+
+module.exports.billCustomer = async (params) => {
+  return "Successfully billed";
+};
+
+module.exports.restoreRedeemPoints = async (params) => {
+  try {
+      if (total.points) {
+        let params = {
+          TableName: 'userTable',
+          Key: { userId: userId },
+          UpdateExpression: 'set points = :points',
+          ExpressionAttributeValues: {
+            ':points': total.points
+          }
+        };
+        await DocumentClient.update(params).promise();
+      }
+    } catch (e) {
+      throw new Error(e);
+  }
+};
+
